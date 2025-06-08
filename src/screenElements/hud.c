31106160af394744a94f05d69d4da49c2c5d28f0
lyoshka <mateusf.2022@alunos.utfpr.edu.br>
2025-06-08 13:23:44 -0300

    new hud

--- src/screenElements/hud.c
+++ src/screenElements/hud.c
@@ -1,9 +1,13 @@
 #include "hud.h"
 
-Sprite *hud_eye_sprite;
-Sprite *hud_eye_text_sprite;
-Sprite *hud_key_sprite;
-Sprite *hud_key_text_sprite;
+GameObject lives_sprite;
+GameObject lives_text;
+GameObject eye_sprite;
+GameObject eye_text_sprite;
+GameObject eye_text_sprite_2;
+GameObject key_sprite;
+GameObject key_text_sprite;
+GameObject key_text_sprite_2;
 
 u16 HUD_init(u16 ind)
 {
@@ -13,38 +17,58 @@ u16 HUD_init(u16 ind)
     VDP_drawImageEx(WINDOW, &img_hud, TILE_ATTR_FULL(PAL_GAME, 1, 0, 0, ind), 0, 0, FALSE, DMA);
     ind += img_hud.tileset->numTile;
 
-    hud_eye_sprite = SPR_addSprite(&spr_hud_itens, 32, 0, TILE_ATTR(PAL_GAME, 1, 0, 0));
-    hud_eye_text_sprite = SPR_addSprite(&spr_font_x, 48, 0, TILE_ATTR(PAL_GAME, 1, 0, 0));
-    hud_key_sprite = SPR_addSprite(&spr_hud_itens, 80, 0, TILE_ATTR(PAL_GAME, 1, 0, 0));
-    hud_key_text_sprite = SPR_addSprite(&spr_font_x, 96, 0, TILE_ATTR(PAL_GAME, 1, 0, 0));
+    ind += GAMEOBJECT_init(&lives_sprite, &spr_hud_itens, 16, 0, PAL_GAME, true, ind);
+    ind += GAMEOBJECT_init(&lives_text, &spr_font_x, 32, 0, PAL_GAME, true, ind);
 
-    SPR_setAnim(hud_eye_text_sprite, 1);
-    SPR_setAnim(hud_key_sprite, 1);
-    SPR_setAnim(hud_key_text_sprite, player_have_key);
+    ind += GAMEOBJECT_init(&eye_sprite, &spr_hud_itens, 64, 0, PAL_GAME, true, ind);
+    ind += GAMEOBJECT_init(&eye_text_sprite, &spr_font_N, 80, 0, PAL_GAME, true, ind);
+    ind += GAMEOBJECT_init(&eye_text_sprite_2, &spr_font_L, 96, 0, PAL_GAME, true, ind);
+
+    ind += GAMEOBJECT_init(&key_sprite, &spr_hud_itens, 128, 0, PAL_GAME, true, ind);
+    ind += GAMEOBJECT_init(&key_text_sprite, &spr_font_N, 144, 0, PAL_GAME, true, ind);
+    ind += GAMEOBJECT_init(&key_text_sprite_2, &spr_font_L, 160, 0, PAL_GAME, true, ind);
+
+    SPR_setAnim(lives_sprite.sprite, 2);
+    SPR_setAnim(lives_text.sprite, 4);
+
+    SPR_setAnim(eye_sprite.sprite, 0);
+    SPR_setAnim(eye_text_sprite.sprite, 0);
+    SPR_setAnim(eye_text_sprite_2.sprite, 3);
+
+    SPR_setAnim(key_sprite.sprite, 1);
+    SPR_setAnim(key_text_sprite.sprite, player_have_key);
+    SPR_setAnim(key_text_sprite_2.sprite, 4);
 
     return ind;
 }
 
 void HUD_update()
 {
-    SPR_setAnim(hud_eye_text_sprite, 1);
-    SPR_setAnim(hud_key_sprite, 1);
-    SPR_setAnim(hud_key_text_sprite, player_have_key);
+    SPR_setAnim(lives_sprite.sprite, 2);
+    SPR_setAnim(lives_text.sprite, 4);
+
+    SPR_setAnim(eye_sprite.sprite, 0);
+    SPR_setAnim(eye_text_sprite.sprite, 0);
+    SPR_setAnim(eye_text_sprite_2.sprite, 3);
+
+    SPR_setAnim(key_sprite.sprite, 1);
+    SPR_setAnim(key_text_sprite.sprite, player_have_key);
+    SPR_setAnim(key_text_sprite_2.sprite, 4);
+    // SPR_setAnim(eye_text_sprite, 1);
+    // SPR_setAnim(key_sprite, 1);
+    // SPR_setAnim(key_text_sprite, player_have_key);
 }
 
 void HUD_clear()
 {
-    if (hud_eye_sprite)
-        SPR_releaseSprite(hud_eye_sprite);
-    if (hud_eye_text_sprite)
-        SPR_releaseSprite(hud_eye_text_sprite);
-    if (hud_key_sprite)
-        SPR_releaseSprite(hud_key_sprite);
-    if (hud_key_text_sprite)
-        SPR_releaseSprite(hud_key_text_sprite);
+    // SPR_releaseSprite(eye_sprite);
+    // SPR_releaseSprite(eye_text_sprite);
 
-    hud_eye_sprite = NULL;
-    hud_eye_text_sprite = NULL;
-    hud_key_sprite = NULL;
-    hud_key_text_sprite = NULL;
+    // SPR_releaseSprite(key_sprite);
+    // SPR_releaseSprite(key_text_sprite);
+
+    // eye_sprite = NULL;
+    // eye_text_sprite = NULL;
+    // key_sprite = NULL;
+    // key_text_sprite = NULL;
 }
